651abb795fc9d18ee0b3ba42a421693b7d839e29
Heshane-11 <hishu3851@gmail.com>
2026-03-01 12:43:28 +0530

    Time: 35 ms (23.97%) | Memory: 23.6 MB (81.32%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,34 +4,33 @@ public:
         int n = grid.size();
         if(n == 0) return -1;
         if(grid[0][0] == 1) return -1;
-        int ans = 1;
         vector<pair<int,int>> directions = {
             {-1,-1}, {-1,0}, {-1,1},
             {0,-1},           {0,1},
             {1,-1},  {1,0},   {1,1}
         };
-        queue<pair<int,int>>q;
-        q.push({0,0});
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, 
+        greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        pq.push({1, {0,0}});
         grid[0][0] = 1;
-        while(!q.empty()){
-            int a = q.size();
-            while(a--){
-                auto [x,y] = q.front();
-                q.pop();
-                if (x == n-1 && y == n-1)
-                    return ans;
-                for(int i = 0; i < directions.size(); i++){
-                    int dx = directions[i].first;
-                    int dy = directions[i].second;
-                    int fx = x+dx;
-                    int fy = y+dy;
-                    if(fx >= 0 && fx < n && fy >= 0 && fy < n && grid[fx][fy] == 0){
-                        q.push({fx,fy});
-                        grid[fx][fy] = 1;
-                    }
-                }
+        dist[0][0] = 1;
+        while(!pq.empty()){
+            auto [d, p] = pq.top();
+            auto [x, y] = p;
+            pq.pop();
+            if(x == n-1 && y == n-1){
+                return d;
             }
-            ans++;
+            for(int i = 0; i < directions.size(); i++){ 
+                auto[dx, dy] = directions[i];
+                int fx = x+dx; int fy = y+dy; 
+                if(fx >= 0 && fx < n && fy >= 0 && fy < n && grid[fx][fy] == 0){
+                    dist[fx][fy] = 1 + dist[x][y]; 
+                    pq.push({dist[fx][fy], {fx,fy}}); 
+                    grid[fx][fy] = 1; 
+                } 
+            }
         }
         return -1;
     }
