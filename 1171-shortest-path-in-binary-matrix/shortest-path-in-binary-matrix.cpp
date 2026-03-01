066c1d0ca1f9ab117a3e546bba62849d39238829
Heshane-11 <hishu3851@gmail.com>
2026-03-01 12:20:43 +0530

    Time: 9 ms (91.49%) | Memory: 23.5 MB (81.32%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
         int n = grid.size();
-
-        // Start or end blocked
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
-            return -1;
-
-        vector<vector<int>> dir = {
-            {-1,-1},{-1,0},{-1,1},
-            {0,-1},{0,1},
-            {1,-1},{1,0},{1,1}
+        if(n == 0) return -1;
+        if(grid[0][0] == 1) return -1;
+        int ans = 1;
+        vector<pair<int,int>> directions = {
+            {-1,-1}, {-1,0}, {-1,1},
+            {0,-1},           {0,1},
+            {1,-1},  {1,0},   {1,1}
         };
-
-        queue<pair<int,int>> q;
+        queue<pair<int,int>>q;
         q.push({0,0});
-        grid[0][0] = 1;  // mark visited
-
-        int path = 1;
-
-        while (!q.empty()) {
-
-            int size = q.size();
-
-            while (size--) {
+        grid[0][0] = 1;
+        while(!q.empty()){
+            int a = q.size();
+            while(a--){
                 auto [x,y] = q.front();
                 q.pop();
-
                 if (x == n-1 && y == n-1)
-                    return path;
-
-                for (auto &d : dir) {
-                    int nx = x + d[0];
-                    int ny = y + d[1];
-
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny] == 0) {
-                        q.push({nx, ny});
-                        grid[nx][ny] = 1;  // mark visited
+                    return ans;
+                for(int i = 0; i < directions.size(); i++){
+                    int dx = directions[i].first;
+                    int dy = directions[i].second;
+                    int fx = x+dx;
+                    int fy = y+dy;
+                    if(fx >= 0 && fx < n && fy >= 0 && fy < n && grid[fx][fy] == 0){
+                        q.push({fx,fy});
+                        grid[fx][fy] = 1;
                     }
                 }
             }
-
-            path++;
+            ans++;
         }
-
         return -1;
     }
 };
