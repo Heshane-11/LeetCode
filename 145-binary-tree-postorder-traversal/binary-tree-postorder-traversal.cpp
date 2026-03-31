adfeb6a03dbea4bf46bd94f9c1cc806f105ea749
Heshane-11 <hishu3851@gmail.com>
2026-03-31 16:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (72.85%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -5,34 +5,32 @@ public:
             return;
         }
         traverse(root->left, ans);
-        
         traverse(root->right, ans);
         ans.push_back(root->val);
     }
 
-    vector<int> postorderTraversal(TreeNode* root) {
+   vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if(root == nullptr){
-            return ans;
-        }
-        stack<TreeNode*> st1;
-        stack<TreeNode*> st2;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* node = st1.top();
-            st1.pop();
-            st2.push(node);
-            if(node->left){
-                st1.push(node->left);
+        stack<TreeNode*> st;
+        TreeNode* lastVisited = nullptr;
+
+        while (root || !st.empty()) {
+            if (root) {
+                st.push(root);
+                root = root->left;
+            } else {
+                TreeNode* node = st.top();
+
+                if (node->right && lastVisited != node->right) {
+                    root = node->right;
+                } else {
+                    ans.push_back(node->val);
+                    lastVisited = node;
+                    st.pop();
+                }
             }
-            if(node->right){
-                st1.push(node->right);
-            }
         }
-        while(!st2.empty()){
-            ans.push_back(st2.top()->val);
-            st2.pop();
-        }
+
         return ans;
     }
 };
