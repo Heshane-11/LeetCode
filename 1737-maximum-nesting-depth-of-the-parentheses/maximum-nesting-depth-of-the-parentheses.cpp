9f42178206cc80a644a5162e1f755884ef0bf269
Heshane-11 <hishu3851@gmail.com>
2026-05-02 14:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (99.04%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,16 +1,38 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         stack<char> st;
+//         int ans = 0;
+
+//         for(int i = 0; i < s.size(); i++){
+//             if(s[i] == '('){
+//                 st.push(s[i]);
+//                 ans = max(ans, (int)st.size()); // update depth
+//             }
+//             else if(s[i] == ')'){
+//                 if(!st.empty()) st.pop();
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> st;
+        int depth = 0;
         int ans = 0;
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push(s[i]);
-                ans = max(ans, (int)st.size()); // update depth
+        for(char c : s){
+            if(c == '('){
+                depth++;
+                ans = max(ans, depth);
             }
-            else if(s[i] == ')'){
-                if(!st.empty()) st.pop();
+            else if(c == ')'){
+                depth--;
             }
         }
 
