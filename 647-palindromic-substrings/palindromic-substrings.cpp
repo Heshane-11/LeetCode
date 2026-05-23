c77e618611ebf02b91e65526fa334960df4a050e
Heshane-11 <hishu3851@gmail.com>
2026-05-23 11:19:17 +0530

    Time: 43 ms (21.92%) | Memory: 11.2 MB (42.73%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    bool palin(string& s, int i, int j){
+    bool palin(string& s, int i, int j, vector<vector<bool>>& dp){
         if(i > j){
             return true;
         }
-        if(s[i] == s[j]){
-            return palin(s, i+1, j-1);
+        if(dp[i][j]){
+            return true;
         }
-        return false;
+        if(s[i] == s[j]){
+            return dp[i][j] = palin(s, i+1, j-1, dp);
+        }
+        return dp[i][j] = false;
     }
     int countSubstrings(string s) {
         int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
         int cnt = 0;
         for(int i = 0; i < n; i++){
             for(int j  = i; j < n; j++){
-                if(palin(s, i, j)){
+                if(palin(s, i, j, dp)){
                     cnt++;
                 }
             }
