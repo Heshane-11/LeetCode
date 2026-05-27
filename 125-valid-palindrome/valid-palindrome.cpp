03dc673dfa9d2bf46dffe7acdac5b849bf269f38
Heshane-11 <hishu3851@gmail.com>
2026-05-27 12:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (30.57%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    char low(char c){
+        if(c >= 'A' && c <= 'Z'){
+            return tolower(c);
+        }
+        return c;
+    }
+    bool isPalindrome(string s) {
+        string ans = "";
+        for(int i = 0; i < s.length(); i++){
+            if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')){
+                ans += low(s[i]);
+            }
+        }
+        int i = 0, j = ans.length()-1;
+        while(i < j){
+            if(ans[i] != ans[j]){
+                return false;
+            }
+            i++;
+            j--; 
+        }
+        return true;
+    }
+};
