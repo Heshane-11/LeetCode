56b64e01b2890c61834e57c332dfcd5fdc675b0f
Heshane-11 <hishu3851@gmail.com>
2026-07-04 14:07:42 +0530

    Time: 5 ms (64.52%) | Memory: 128.2 MB (95.35%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,26 +1,45 @@
+/*brute force:
+make two diff arrays of pos and neg numbers
+merge them in nums acc. to the condition given
+*/
+
+
+/*
+
+Optimal Approach (O(n) Time, O(n) Space)
+
+Observation:
+
+Result array mein positive numbers even indices par aayenge: 0, 2, 4, ...
+Negative numbers odd indices par aayenge: 1, 3, 5, ...
+Order preserve karna hai.
+Algorithm (2 lines)
+Create an answer array of size n and maintain two pointers: pos = 0 and neg = 1.
+Traverse nums; place positive numbers at ans[pos] and negative numbers at ans[neg], then increment the respective pointer by 2.
+
+*/
+
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> p;
-        vector<int> n;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] > 0){
-                p.push_back(nums[i]);
+        int n = nums.size();
+
+        vector<int> ans(n);
+
+        int pos = 0;
+        int neg = 1;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+            else {
+                ans[neg] = nums[i];
+                neg += 2;
             }
         }
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] < 0){
-                n.push_back(nums[i]);
-            }
-        }
-        for(int i = 0; i < nums.size(); i++){
-            if(i % 2 == 0){
-                nums[i] = p[i/2];
-            }
-            else{
-                nums[i] = n[i/2];
-            }
-        }
-        return nums;
+
+        return ans;
     }
 };
