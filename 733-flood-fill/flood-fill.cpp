44efab9bbafeea07c2e971a3c760331f185692af
Heshane-11 <hishu3851@gmail.com>
2026-07-25 10:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (44.27%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int r = image.size(), c = image[0].size();
-        vector<vector<int>> res = image;
-
-        int orig = image[sr][sc];
-        if(orig == color) return res;
-
+        int m = image.size();
+        int n = image[0].size();
+        int org = image[sr][sc];
+        if (org == color)
+            return image;
         queue<pair<int,int>> q;
         q.push({sr, sc});
-        res[sr][sc] = color;
-
+        vector<int> dirx = {0, 0, -1, 1};
+        vector<int> diry = {1, -1, 0, 0};
+        image[sr][sc] = color;
         while(!q.empty()){
-            auto node = q.front();
+            auto[x,y] = q.front();
             q.pop();
-
-            int i = node.first;
-            int j = node.second;
-
-            // up
-            if(i > 0 && res[i-1][j] != color && image[i-1][j] == orig){
-                q.push({i-1, j});
-                res[i-1][j] = color;
-            }
-            // down
-            if(i < r-1 && res[i+1][j] != color && image[i+1][j] == orig){
-                q.push({i+1, j});
-                res[i+1][j] = color;
-            }
-            // left
-            if(j > 0 && res[i][j-1] != color && image[i][j-1] == orig){
-                q.push({i, j-1});
-                res[i][j-1] = color;
-            }
-            // right
-            if(j < c-1 && res[i][j+1] != color && image[i][j+1] == orig){
-                q.push({i, j+1});
-                res[i][j+1] = color;
+            for(int i = 0; i < 4; i++){
+                int nx = x + dirx[i];
+                int ny = y + diry[i];
+                if(nx >= 0 && nx < m && ny >= 0 && ny < n && image[nx][ny] == org){
+                    image[nx][ny] = color;
+                    q.push({nx, ny});
+                }
             }
         }
-        return res;
+        return image;
     }
 };
