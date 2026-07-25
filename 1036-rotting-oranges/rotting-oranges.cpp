e9c3b0828fb0231075e2967688104d004b4b7ca1
Heshane-11 <hishu3851@gmail.com>
2026-07-25 10:23:24 +0530

    Time: 1 ms (36.94%) | Memory: 16.6 MB (86.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,61 +1,47 @@
+/*
+DFS is problem ke liye suitable nahi hai kyunki wo ek direction mein depth tak chala jata hai aur kisi cell tak pahunchne ka minimum time guarantee nahi karta. Rotting process har minute sabhi rotten oranges se ek saath (simultaneously) spread hoti hai, jo level-by-level hoti hai. Isi wajah se Multi-Source BFS use karte hain, kyunki BFS har level ko ek minute maan kar har cell tak minimum time mein pahunchne ki guarantee deta hai.
+
+*/
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
-        queue<pair<int,int>> q;
-        int cnt = 0;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) {
-                    cnt++;
+        int fresh = 0;
+        queue<pair<int, int>> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    fresh++;
                 }
-                else if(grid[i][j] == 2) {
+                else if(grid[i][j] == 2){
                     q.push({i, j});
                 }
             }
         }
-
-        int t = 0;
-
-        int dx[4] = {-1, 1, 0, 0};
-        int dy[4] = {0, 0, -1, 1};
-
-        while(!q.empty()) {
+        vector<int> dirx = {0, 0, -1, 1};
+        vector<int> diry = {1, -1, 0, 0};
+        int time = 0;
+        while(!q.empty()){
             int s = q.size();
-            bool rot = false;
-
-            while(s--) {
-                auto curr = q.front();
+            while(s > 0){
+                auto [x,y] = q.front();
                 q.pop();
-
-                int x = curr.first;
-                int y = curr.second;
-
-                for(int k = 0; k < 4; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-
-                    if(nx >= 0 && nx < m &&
-                       ny >= 0 && ny < n &&
-                       grid[nx][ny] == 1) {
-
+                for(int i = 0; i < 4; i++){
+                    int nx = x + dirx[i];
+                    int ny = y + diry[i];
+                    if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1){
                         grid[nx][ny] = 2;
-                        cnt--;
-                        rot = true;
-
+                        fresh--;
                         q.push({nx, ny});
                     }
                 }
+                s--;
             }
-
-            if(rot) {
-                t++;
-            }
+            if(!q.empty())
+                time++;
         }
-
-        return cnt == 0 ? t : -1;
+        return (fresh > 0) ? -1 : time;
     }
 };
