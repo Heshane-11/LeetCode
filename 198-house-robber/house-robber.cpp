175a894623f9b93e4089ac5f3a0a4573194a56f2
Heshane-11 <hishu3851@gmail.com>
2026-08-15 11:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (24.22%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -16,9 +16,16 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        int x = solve(nums, 0, dp);
-        int y = solve(nums, 1, dp);
-        return max(x, y);
+        if(n == 1){
+            return nums[0];
+        }
+        
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
